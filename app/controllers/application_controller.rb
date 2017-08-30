d9f2974b74208d63cba4c8ecae212fef2195bf97
hansenjl <jenn.leigh.hansen@gmail.com>
2017-08-30 13:11:49 -0700

    Create sign up and log in forms for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user_id
+
+  def current_user_id
+    session[:user_id]
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,13 @@
 class SessionsController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
+  def create
+
+  end
+
   def destroy
     session.delete :user_id
   end
