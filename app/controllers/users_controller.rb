7b6b1d0af37313f1ed6e1c8413c04831fdcdbaf6
hansenjl <jenn.leigh.hansen@gmail.com>
2017-08-30 13:16:16 -0700

    Update sessions controller and user controller to create a new user or session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,21 @@
 class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to user_path(user)
+    else
+      #error message
+      redirect_to new_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :name :password, :password_confirmation)
+  end
 
 end
