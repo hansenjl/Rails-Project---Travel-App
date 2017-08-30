46df56a0ee60e35f1e641c87d6f418edaccb082e
hansenjl <jenn.leigh.hansen@gmail.com>
2017-08-30 12:10:27 -0700

    Create relationships between models

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,6 @@
 class City < ApplicationRecord
+  has_many :visits
+  has_many :users, through: :visits
+  has_many :reviews
+  belongs_to :country
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :visits
+  has_many :cities, through: :visits
+  has_many :reviews
 end
