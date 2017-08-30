29f10ece120e37f7433b19009bd425c302e9dc2a
hansenjl <jenn.leigh.hansen@gmail.com>
2017-08-30 13:55:14 -0700

    Add signup route and checkoff spec items

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
 
   resources :countries
   resources :visits
