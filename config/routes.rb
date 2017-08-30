7c8cb65e8c28ab9f4e68a3e5d3e175800b4e3ead
hansenjl <jenn.leigh.hansen@gmail.com>
2017-08-30 12:26:38 -0700

    Create view files and routes for all models

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  root 'cities#index'
+
   resources :countries
-  resources :reviews
+  resources :sessions
   resources :visits
-  resources :cities
+  resources :cities do
+    resources :reviews
+  end
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
