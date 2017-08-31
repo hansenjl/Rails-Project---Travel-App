f30e3dd630f5e49a843130387e06137d1a9fccce
hansenjl <jenn.leigh.hansen@gmail.com>
2017-08-31 10:42:23 -0700

    Redirect after creating a new visit

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -10,23 +10,12 @@ class VisitsController < ApplicationController
 
   def create
     binding.pry
-    if params[:visit][:city_name]
-      #user entered city takes precedence
-      if params[:visit][:country]
-        #user entered new country takes precedence
-      else
-        #country is chosen from drop down list
-        # in this case, a new city must be created, a new country must be created, and then the visit can be created
-      end
-
+    visit = Visit.new(visit_params)
+    if visit.save
+      redirect_to user_visits_path(user_id: params[:user_id])
     else
-      #city is selected from drop down list
-      Visit.new(city_id: params[:visit][:city_id], user_id: current_user.id, rating: params[:visit][:rating])
+      redirect_to new_user_visit(user_id: params[:user_id])
     end
-
-
-    binding.pry
-
   end
 
   private
