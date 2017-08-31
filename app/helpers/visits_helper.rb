a6d7a20bfaaf7555eeabbd57f67c31fd2ab96169
hansenjl <jenn.leigh.hansen@gmail.com>
2017-08-30 17:32:13 -0700

    Create new visit form and seed data

--- app/helpers/visits_helper.rb
+++ app/helpers/visits_helper.rb
@@ -1,2 +1,6 @@
 module VisitsHelper
+
+  def new
+    @countries = Country.all
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Country.create(name: "United States")
+Country.create(name: "Tanzania")
+Country.create(name: "Canada")
