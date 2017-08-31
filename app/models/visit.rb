6ff5fdca75e33e5f48f6d89122c5827112c18a34
hansenjl <jenn.leigh.hansen@gmail.com>
2017-08-31 11:33:51 -0700

    Change city_id to a city attribute under the new visit form

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,4 +1,21 @@
 class Visit < ApplicationRecord
   belongs_to :user
   belongs_to :city
+
+  def city_attributes=(city_attributes)
+    if !city_attributes[:name].nil?
+      city = City.find_or_create_by(name: city_attributes[:name])
+      city_attributes[:city_id] = city.id
+    else
+      city = City.find_by(id: city_attributes[:city_id])
+    end
+    if !city_attributes[:country].nil?
+      country = Country.find_or_create_by(name: city_attributes[:country])
+      city_attributes[:country_id] = country.id
+    else
+      country = Country.find_by(id: city_attributes[:country_id])
+    end
+      country.cities << city
+
+  end
 end
