1d3a5777c283d93c5957375c7c7a272b4a9accbd
hansenjl <jenn.leigh.hansen@gmail.com>
2017-08-30 17:19:58 -0700

    Create user show view and define the overall rating for a city

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,4 +3,13 @@ class City < ApplicationRecord
   has_many :users, through: :visits
   has_many :reviews
   belongs_to :country
+
+  def overall_rating
+    total = 0
+    self.visits.each do |visit|
+      total = total + visit.rating
+    end
+    total
+  end
+
 end
