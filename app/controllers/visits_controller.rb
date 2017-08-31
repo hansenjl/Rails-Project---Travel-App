6a7540a772d547f1b41463e0f8b30c99a959b605
hansenjl <jenn.leigh.hansen@gmail.com>
2017-08-31 09:45:45 -0700

    Added a ratings field to the visit form and started creating the controller action for visit#create

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,7 +1,34 @@
+require 'pry'
 class VisitsController < ApplicationController
 
   def new
     @cities = City.all
     @countries = Country.all
+  end
+
+  def create
+    if params[:visit][:city_name]
+      #user entered city takes precedence
+      if params[:visit][:country]
+        #user entered new country takes precedence
+      else
+        #country is chosen from drop down list
+        # in this case, a new city must be created, a new country must be created, and then the visit can be created
+      end
+
+    else
+      #city is selected from drop down list
+      Visit.new(city_id: params[:visit][:city_id], user_id: current_user.id, rating: params[:visit][:rating])
+    end
+
+
+    binding.pry
+
+  end
+
+  private
+
+  def visit_params
+    params.require(:visit).permit(:country, :ciy_id, :city_name, :country_id)
   end
 end
