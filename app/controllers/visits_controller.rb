68a37dd1eba8577f88a2242a277ee68d2605d1d9
hansenjl <jenn.leigh.hansen@gmail.com>
2017-09-13 14:35:14 -0700

    Add set_user before_action for visits controller

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,18 +1,17 @@
 class VisitsController < ApplicationController
   load_and_authorize_resource :user
   load_and_authorize_resource :visit, through: :user
+  before_action :set_user, only: [:new, :index, :create, :destroy]
 
   def new
     @visit = Visit.new
     @cities = City.all
     @countries = Country.all
-    @user = User.find_by(id: params[:user_id])
   end
 
   def create
-    @user = User.find_by(id: params[:user_id])
     @visit = Visit.new(visit_params)
-    @visit.user_id = params[:user_id]
+    binding.pry
     @visit.set_city(params[:visit][:city_attributes], @visit, @user)
     if @visit.save
       redirect_to user_visits_path(user_id: params[:user_id])
@@ -23,7 +22,6 @@ class VisitsController < ApplicationController
   end
 
   def index
-    @user = User.find_by(id: params[:user_id])
     @visits = @user.visits.all
   end
 
@@ -33,6 +31,10 @@ class VisitsController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find_by(id: params[:user_id])
+  end
 
   def visit_params
     params.require(:visit).permit(:user_id, :city_id, :city_rating, :city_attributes)
