29914fe975bc45a2c547c258430e4ab099c406a5
hansenjl <jenn.leigh.hansen@gmail.com>
2017-09-15 16:02:04 -0700

    Fixed cancancan ability error by removing Session from ability model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
 
-    can :read, [City, Country, User, Session]
+    can :read, [City, Country, User]
     can :create, User
     can :most_visited, Country
 
