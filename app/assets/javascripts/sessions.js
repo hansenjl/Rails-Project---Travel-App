66d72d3b47a163082fcaf3c1a8000de35d6b9f75
hansenjl <jenn.leigh.hansen@gmail.com>
2017-09-25 09:40:10 -0700

    Added comments to display on welcome view

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -2,12 +2,26 @@ document.addEventListener("turbolinks:load", attachListeners)
 
 function attachListeners(){
   if ($("body")[0] === document.getElementsByClassName("sessions welcome")[0]){
-    getComments
+    getComments()
   }
 }
 
 function getComments(){
   $.get("/comments.json", function(data){
-
+    console.log(data)
+    let commentList = ''
+    for (var i = 0; i < data.length; i++) {
+      commentList = commentList + formatComment(data[i])
+    }
+    $("#comment-scroll").html(commentList)
   })
 }
+
+function formatComment(comment){
+  let username = comment["user"]["username"]
+  let text = comment["text"]
+  let city = comment["city"]["name"]
+  return `<li><strong>${city}:</strong>
+          ${username} says <em>"${text}"</em> </li><br></br>`
+}
+
