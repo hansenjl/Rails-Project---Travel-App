c3dcce49c0497e385db8633e1dee4a80aec9caee
hansenjl <jenn.leigh.hansen@gmail.com>
2019-03-08 22:54:44 -0800

    remove rating column from cities

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190308193354) do
+ActiveRecord::Schema.define(version: 20170918153839) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
     t.integer "country_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "avg_rating"
   end
 
   create_table "comments", force: :cascade do |t|
