0cec0cb5321ae918b2342030c2008cd0052de80a
hansenjl <jenn.leigh.hansen@gmail.com>
2019-03-08 23:28:58 -0800

    Increased loading speed of visits/new by preloading counties

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -3,7 +3,7 @@ class VisitsController < ApplicationController
   load_and_authorize_resource :visit, through: :user
   before_action :set_user, only: [:new, :index, :create, :destroy]
 
-
+ #need to rework visits so that it is NOT nested under user but nested under city
   def new
     @visit = Visit.new
   end
@@ -34,8 +34,6 @@ class VisitsController < ApplicationController
   def set_user
     @user = User.find_by(id: params[:user_id])
   end
-
-
 
   def visit_params
     params.require(:visit).permit(:user_id, :city_id, :city_rating, :avatar, city_attributes:[:name,:country_id,:country])
