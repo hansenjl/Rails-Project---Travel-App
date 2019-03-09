3cc62e6199964cae29415645e850b187d6f5befd
hansenjl <jenn.leigh.hansen@gmail.com>
2019-03-08 23:16:07 -0800

    adjust welcome page for new methods and partial edits

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -28,10 +28,6 @@ class SessionsController < ApplicationController
 
   def welcome
     @users = User.active_users.includes(:cities)
-
-
-
-
     @most_visited_city = City.most_visited
     @most_popular_city = City.most_popular
   end
