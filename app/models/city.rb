2d9b9e1135247c09e6456ce6583a2d87cbbdd215
hansenjl <jenn.leigh.hansen@gmail.com>
2019-03-08 22:34:00 -0800

    Add unrated scope method

--- app/models/city.rb
+++ app/models/city.rb
@@ -8,24 +8,24 @@ class City < ApplicationRecord
 
 
 #this might be better as a column on the City table that gets updated when a visit is created/updated
-  def overall_rating
-    self.avg_rating ||= Visit.avg_rating_for_city(self.id)
-    # if self.visited?
-    #   total = 0
-    #   self.visits.each do |visit|
-    #     total = total + visit.city_rating.to_f
-    #   end
-    #   total = total / times_visited.to_f
-    # else
-    #   total = 0
-    # end
-    # total
-  end
+  # def overall_rating
+  #   self.avg_rating ||= Visit.avg_rating_for_city(self.id)
+  #   # if self.visited?
+  #   #   total = 0
+  #   #   self.visits.each do |visit|
+  #   #     total = total + visit.city_rating.to_f
+  #   #   end
+  #   #   total = total / times_visited.to_f
+  #   # else
+  #   #   total = 0
+  #   # end
+  #   # total
+  # end
 
-  def set_avg_rating
-    self.avg_rating = Visit.avg_rating_for_city(self.id)
-    self.save
-  end
+  # def set_avg_rating
+  #   self.avg_rating = Visit.avg_rating_for_city(self.id)
+  #   self.save
+  # end
 
   def name_with_country
     "#{self.name}, #{self.country.name}"
@@ -46,15 +46,27 @@ class City < ApplicationRecord
     self.country_id = country.id if !country.name.empty?
   end
 
+  def self.rated
+      select("cities.*, AVG(visits.city_rating) AS overall_rating").joins(:visits).group("cities.id")
+  end
+
+  def self.unrated
+    left_outer_joins(:visits).where(visits: { id: nil })
+  end
+
 
   def self.most_visited
     #City.all.sort{ |a,b| b.times_visited <=> a.times_visited}.first
      joins(:visits).group(:id).order('count(visits.id) DESC').first
   end
 
-  def self.most_popular
+  def self.popular
     #City.all.sort{ |a,b| b.overall_rating.to_f <=> a.overall_rating.to_f}.first
-    joins(:visits).group(:id).order('avg(visits.city_rating) DESC').first
+    select("cities.*, AVG(visits.city_rating) AS overall_rating").joins(:visits).group(:id).order('overall_rating DESC')
+  end
+
+  def self.most_popular
+    popular.first
   end
 
 end
